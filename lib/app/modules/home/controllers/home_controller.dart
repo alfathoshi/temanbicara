637fd2c60f26a81b7d13389c0526d3aefe51a75a
Fehehehe <febryt22@gmail.com>
2024-12-29 23:26:01 +0700

    hehe maap kehapus import module httpnya

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,23 +2,20 @@ import 'dart:convert';
 
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-
-
+import 'package:http/http.dart' as http;
 
 class HomeController extends GetxController {
   final box = GetStorage();
   var isLoading = false.obs;
 
   Future<Map<String, dynamic>> fetchData() async {
- 
     final response =
         await http.get(Uri.parse('http://10.0.2.2:8000/api/v1/article'));
     if (response.statusCode == 200) {
       return json.decode(response.body);
     } else {
-       print(response.body);
+      print(response.body);
       throw Exception('Failed to load article');
     }
   }
 }
-
