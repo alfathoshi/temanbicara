1d105f691bcb0c39700250b9dee6c398ec3e24e4
Raga-Git <ragadhitya@gmail.com>
2025-05-14 14:55:57 +0700

    test: make coverage test

---
+++ test/main_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:temanbicara/main.dart' as app;
+
+void main() {
+  test('main() runs without throwing', () {
+    expect(() => app.main(), returnsNormally);
+  });
+}
