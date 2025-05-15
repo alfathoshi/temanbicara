fcf9be2528c041d5306fb4b0aabb0db112953e47
Revan Arturito <revan.arturito@gmail.com>
2025-05-16 00:21:21 +0700

    feat: Share Invoice on Transaction Invoice Page

--- lib/app/modules/transaction_invoice/controllers/transaction_invoice_controller.dart
+++ lib/app/modules/transaction_invoice/controllers/transaction_invoice_controller.dart
@@ -4,8 +4,12 @@ import 'package:path_provider/path_provider.dart';
 import 'package:temanbicara/app/data/invoice_model.dart';
 import 'package:temanbicara/app/themes/colors.dart';
 import 'package:temanbicara/app/widgets/transaction/invoice_pdf.dart';
+import 'package:temanbicara/app/utils/platform_channel.dart';
 
 class TransactionInvoiceController extends GetxController {
+  final isSharing = false.obs;
+  final isDownloading = false.obs;
+
   Future<String> getDownloadPath() async {
     if (Platform.isAndroid) {
       final directory = Directory('/storage/emulated/0/Download');
@@ -20,6 +24,8 @@ class TransactionInvoiceController extends GetxController {
 
   Future<void> downloadInvoice(InvoiceModel invoice) async {
     try {
+      isDownloading.value = true;
+
       final downloadDir = await getDownloadPath();
       final filePath =
           '$downloadDir/invoice_${DateTime.now().millisecondsSinceEpoch}.pdf';
@@ -29,16 +35,41 @@ class TransactionInvoiceController extends GetxController {
       Get.snackbar(
         'Success!',
         'Your invoice has been saved to the Download folder.',
-        backgroundColor: primaryColor.withValues(alpha: 0.7),
+        backgroundColor: primaryColor.withOpacity(0.7),
         colorText: whiteColor,
       );
     } catch (e) {
       Get.snackbar(
         'Oops!',
         'Failed to save invoice. Please try again.',
-        backgroundColor: error.withValues(alpha: 0.7),
+        backgroundColor: error.withOpacity(0.7),
         colorText: whiteColor,
       );
+    } finally {
+      isDownloading.value = false;
+    }
+  }
+
+  Future<void> shareInvoice(InvoiceModel invoice) async {
+    try {
+      isSharing.value = true;
+
+      final downloadDir = await getDownloadPath();
+      final filePath =
+          '$downloadDir/invoice_${DateTime.now().millisecondsSinceEpoch}.pdf';
+
+      final file = await CreatePDF.generatePDF(invoice, outputPath: filePath);
+
+      await NativeShare.shareFile(file.path);
+    } catch (e) {
+      Get.snackbar(
+        'Oops!',
+        'Failed to share invoice: $e',
+        backgroundColor: error.withOpacity(0.85),
+        colorText: whiteColor,
+      );
+    } finally {
+      isSharing.value = false;
     }
   }
 }
