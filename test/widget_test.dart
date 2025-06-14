7287b653ac83d6060cd676c3b6ec15265fef5bcc
Muhammad Bintang Al-Fath <alfathbintangmuhammad@gmail.com>
2025-06-15 03:55:31 +0700

    refactor: widget_test with print debug

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:async';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -14,27 +16,31 @@ import 'package:temanbicara/app/routes/app_pages.dart';
 void main() {
   setUpAll(() async {
     TestWidgetsFlutterBinding.ensureInitialized();
-    await GetStorage.init(); // init storage
+    await GetStorage.init();
+    await Future.delayed(const Duration(milliseconds: 100));
   });
 
   testWidgets('Splash screen renders Teman Bicara text',
       (WidgetTester tester) async {
-    // Simulasi GetStorage (mock data)
-    final box = GetStorage();
-    await box.write('firstTime', false);
-    await box.write('token', null); // biar route tetep stay di splash
+    await runZonedGuarded(() async {
+      final box = GetStorage();
+      await box.write('firstTime', false);
+      await box.write('token', null);
 
-    await tester.pumpWidget(
-      GetMaterialApp(
-        home: const SplashScreenView(),
-        getPages: AppPages.routes,
-      ),
-    );
+      await tester.pumpWidget(
+        GetMaterialApp(
+          home: const SplashScreenView(),
+          getPages: AppPages.routes,
+        ),
+      );
 
-    await tester.pump(const Duration(seconds: 3)); // biar future.delayed kelar
-    await tester.pumpAndSettle(); // tunggu animasi & route selesai
+      await tester.pump(const Duration(seconds: 3));
+      await tester.pumpAndSettle();
 
-    // Expect teks muncul
-    expect(find.text('Teman Bicara'), findsOneWidget);
+      expect(find.text('Teman Bicara'), findsOneWidget);
+    }, (e, s) {
+      print("ERROR IN TEST: $e\n$s");
+    });
   });
 }
+
