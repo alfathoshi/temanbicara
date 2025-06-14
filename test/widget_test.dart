81612e70c07eac19db695abd3c7322f92112bed9
Muhammad Bintang Al-Fath <alfathbintangmuhammad@gmail.com>
2025-06-15 04:54:06 +0700

    refactor: add mockito to flutter test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,55 +6,41 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:mockito/mockito.dart';
+import 'package:temanbicara/app/modules/splash_screen/views/splash_screen_view.dart';
+import 'package:temanbicara/app/routes/app_pages.dart';
 
-class FakeImageWidget extends StatelessWidget {
-  final double? width;
-  final double? height;
-
-  const FakeImageWidget({this.width, this.height, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: width ?? 100,
-      height: height ?? 100,
-      color: Colors.grey,
-    );
-  }
-}
-
-class DummyGetStorage implements GetStorage {
-  final Map<String, dynamic> _store = {};
-
-  @override
-  T? read<T>(String key) => _store[key] as T?;
-
-  @override
-  Future<void> write(String key, dynamic value) async {
-    _store[key] = value;
-  }
-
-  @override
-  bool hasData(String key) => _store.containsKey(key);
-
-  // Optional: implement other GetStorage members if needed
-  @override
-  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
-}
+class MockGetStorage extends Mock implements GetStorage {}
 
 void main() {
-  testWidgets('Sanity check - Hello World renders', (tester) async {
+  late MockGetStorage mockStorage;
+
+  setUp(() {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    Get.testMode = true;
+    mockStorage = MockGetStorage();
+
+    when(mockStorage.read('firstTime')).thenReturn(false);
+    when(mockStorage.read('token')).thenReturn(null);
+
+    Get.put<GetStorage>(mockStorage);
+  });
+
+  testWidgets('Splash screen test', (tester) async {
     await tester.pumpWidget(
-      const MaterialApp(
-        home: Scaffold(
-          body: Text('Hello World'),
-        ),
+      GetMaterialApp(
+        home: SplashScreenView(),
+        getPages: AppPages.routes,
       ),
     );
 
-    expect(find.text('Hello World'), findsOneWidget);
+    await tester.pump(const Duration(seconds: 3));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Teman Bicara'), findsOneWidget);
   });
 }
+
