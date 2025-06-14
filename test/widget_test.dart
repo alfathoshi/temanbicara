b95c8c03a646c5f97466484a872ee93e300d7651
Muhammad Bintang Al-Fath <alfathbintangmuhammad@gmail.com>
2025-06-15 03:15:41 +0700

    fix: widget_test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -13,17 +12,13 @@ import 'package:temanbicara/app/modules/splash_screen/controllers/splash_screen_
 import 'package:temanbicara/app/modules/splash_screen/views/splash_screen_view.dart';
 
 void main() {
-  setUpAll(() async {
-    await GetStorage.init();
-  });
-
-  setUp(() async {
-    final box = GetStorage();
-    await box.erase();
-  });
-
   testWidgets('Splash screen renders Teman Bicara text',
       (WidgetTester tester) async {
+    await GetStorage.init();
+
+    final box = GetStorage();
+    await box.erase();
+
     Get.put(SplashScreenController());
 
     await tester.pumpWidget(
