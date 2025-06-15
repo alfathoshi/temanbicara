3eb101e9d7f17a2823ccb2cf930a84948185fe08
Muhammad Bintang Al-Fath <alfathbintangmuhammad@gmail.com>
2025-06-15 19:50:47 +0700

    update: release workflow

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,17 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:temanbicara/app/modules/splash_screen/views/splash_screen_view.dart';
+import 'package:temanbicara/app/routes/app_pages.dart';
 
 void main() {
-  testWidgets('Teman Bicara app renders splash screen', (tester) async {
+  testWidgets('Teman Bicara app renders and routes to splash screen', (tester) async {
     await tester.pumpWidget(
-      const GetMaterialApp(
-        home: Scaffold(
-          body: Text('Teman Bicara'),
-        ),
+      GetMaterialApp(
+        initialRoute: Routes.SPLASH_SCREEN,
+        getPages: AppPages.routes,
       ),
     );
 
-    expect(find.text('Teman Bicara'), findsOneWidget);
+    expect(find.byType(SplashScreenView), findsOneWidget);
   });
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,34 +1,7 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
-import 'package:mockito/mockito.dart';
-import 'package:temanbicara/app/modules/splash_screen/controllers/splash_screen_controller.dart';
 import 'package:temanbicara/app/modules/splash_screen/views/splash_screen_view.dart';
-import 'package:temanbicara/app/utils/localstorage.dart';
-
-class MockLocalStorage extends Mock implements LocalStorage {}
-
-class FakeSplashScreenController extends SplashScreenController {
-  FakeSplashScreenController()
-      : super(
-          storage: MockLocalStorage(),
-          delay: Duration.zero,
-          navigate: (_, {arguments}) {},
-        );
-
-  @override
-  void handleRouting() {}
-
-  @override
-  void onReady() {}
-}
 
 void main() {
   testWidgets('Splash shows logo and text', (tester) async {
@@ -39,6 +12,6 @@ void main() {
     );
 
     expect(find.text('Teman Bicara'), findsOneWidget);
-    expect(find.byType(FlutterLogo), findsOneWidget);
+    expect(find.byType(Image), findsOneWidget);
   });
 }
